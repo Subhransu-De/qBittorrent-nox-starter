69911f5c24016a275f2877095c6b7d89953e9687
Subhransu De <subhransu.de.official@gmail.com>
2025-02-09 19:15:24 +0530

    Add extra logs

--- start.sh
+++ start.sh
@@ -3,4 +3,16 @@ echo "┗┓╋┏┓┏┓╋┓┏┓┏┓   ";
 echo "┗┛┗┗┻┛ ┗┗┛┗┗┫•••";
 echo "            ┛   ";
 
-openssl req -newkey rsa:2048 -nodes -keyout ./config/server.key -x509 -days 365 -out ./config/server.crt -config openssl.conf && docker compose --env-file .env up
+echo "Starting qBittorrent-nox with Docker Compose..."
+echo "Generating SSL certificate..."
+
+openssl req -newkey rsa:2048 -nodes -keyout ./config/server.key -x509 -days 365 -out ./config/server.crt -config openssl.conf
+
+if [ $? -eq 0 ]; then
+    echo "SSL certificate generated successfully."
+    echo "Starting Docker Compose..."
+    docker compose --env-file .env up
+else
+    echo "SSL certificate generation failed."
+    exit 1
+fi
